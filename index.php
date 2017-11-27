6cd2e58666580fdafb161d1443921a4809caf27f
Sankara <serge.maikere@gmail.com>
2017-11-27 15:01:07 +0100

    Navigation close to cmopletion + social sidebar done

--- index.php
+++ index.php
@@ -7,27 +7,102 @@
 		<!-- BOOTSTRAP -->
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
 		<!-- JQuery -->
 		<script src="http://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<!-- My CSS -->
+		<link rel="stylesheet" type="text/css" href="css/style.css">
 
 	</head>
 	<body>
 
 		<div class="container-fluid">
 
-			<section id="top">
+			<section id="logo">
+
+				<div class="row">
+					
+				</div>
 					
 			</section>
 
 			<section id="navigation-bar">
-				
+
+				<div id="navbar">    
+				  <nav class="navbar navbar-default navbar-static-top" role="navigation">
+				            <div class="navbar-header">
+				                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
+				                    <span class="sr-only">Toggle navigation</span>
+				                    <span class="icon-bar"></span>
+				                    <span class="icon-bar"></span>
+				                    <span class="icon-bar"></span>
+				                </button>
+				            </div>
+				            
+				            <div class="collapse navbar-collapse" id="navbar-collapse-1">
+
+				                <ul class="nav navbar-nav text-center">
+									
+				                    <li class="empty-lg"></li>
+									
+				                    <li><a href="#">HOME</a></li>
+
+				                    <li class="dropdown">
+				                      <a href="#" class="dropdown-toggle" data-toggle="dropdown">Films <b class="caret"></b></a> 
+				                      
+				                        <ul class="dropdown-menu">
+				                            <li><a href="#">Action</a></li>
+				                            <li><a href="#">Adventure</a></li>
+				                            <li><a href="#">Drama</a></li>                                   
+				                        </ul>
+
+				                    </li>
+
+				                    <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Séries<b class="caret"></b></a>
+
+				                        <ul class="dropdown-menu">
+				                            <li><a href="#">Action</a></li>
+				                            <li><a href="#">Adventure</a></li>
+				                            <li><a href="#">Drama</a></li>	                                                           
+				                        </ul>
+
+				                    </li>
+
+				                    <li class="empty"></li>
+				                    <li class="empty"></li>
+				                    <li class="empty"></li>
+				                    <li class="empty"></li>
+
+				                    <li class="empty-lg"></li>
+
+
+				                </ul>
+
+				            </div><!-- /.navbar-collapse -->
+				        </nav>
+				</div>
+
 			</section>
+
+				<div class="jumbotron text-center">
+					<h1>Lorem Ipsum Rocks !</h1>
+				</div>
 
 			<header>
 				
 			</header>
+
+			<aside id="mySidenav" class="sidenav">
+
+				<a href="#" id="about">Facebook</a>
+				<a href="#" id="blog">Twitter</a>
+				<a href="#" id="projects">Instagram</a>
+				<a href="#" id="contact">Contact</a>
+
+			</aside>
+		
 				
 			<main class="container">
 				
